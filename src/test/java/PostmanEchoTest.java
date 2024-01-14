12070940f6450f0a336afd7f9c4362bae22f52f8
Korolev E <nuuchcha@bk.ru>
2024-01-14 18:48:48 +0500

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnData() {
+        var text = "Hallo";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"));
+    }
+}
